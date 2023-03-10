c22b33c53ef15140d62e0a6771de86a88b49deb7
butthx <68604099+butthx@users.noreply.github.com>
2023-03-10 19:04:19 +0700

    add new line after double space in changelog

---
+++ generator/chglog/index.js
@@ -0,0 +1,16 @@
+/**
+ * tgsnake - Telegram MTProto framework for nodejs.
+ * Copyright (C) 2023 butthx <https://github.com/butthx>
+ *
+ * THIS FILE IS PART OF TGSNAKE
+ *
+ * tgsnake is a free software : you can redistribute it and/or modify
+ * it under the terms of the MIT License as published.
+ */
+
+const fs = require('fs');
+const path = require('path');
+
+const file = fs.readFileSync(path.join(__dirname, '../CHANGELOG.md'), 'utf8');
+const content = file.replace(/\s{2,}/gm, '  \n');
+fs.writeFileSync(path.join(__dirname, '../CHANGELOG.md'), 'content');
