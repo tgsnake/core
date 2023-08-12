e57604b94ec435ab5c943d685f9cb454349b9ce3
butthx <68604099+butthx@users.noreply.github.com>
2023-08-12 13:51:31 +0700

    fix: generator to sync latest layer

--- generator/sync-tl/index.js
+++ generator/sync-tl/index.js
@@ -8,30 +8,45 @@
  * it under the terms of the MIT License as published.
  */
 
+const notify = require('../notify');
 const fs = require('fs');
 const path = require('path');
 
 async function getTl() {
   const readme = fs.readFileSync(path.join(__dirname, '../../README.md'), 'utf8');
   const prev = fs.readFileSync(path.join(__dirname, '../api/source/api.tl'), 'utf8');
-  const res = await fetch(
-    'https://raw.githubusercontent.com/telegramdesktop/tdesktop/dev/Telegram/Resources/tl/api.tl',
-    {
-      method: 'GET',
-      mode: 'cors',
-    }
-  );
-  const tl = await res.text();
+  const layer = await (
+    await fetch(
+      'https://raw.githubusercontent.com/telegramdesktop/tdesktop/dev/Telegram/SourceFiles/mtproto/scheme/layer.tl',
+      {
+        method: 'GET',
+        mode: 'cors',
+      }
+    )
+  ).text();
+  const tl =
+    (await (
+      await fetch(
+        'https://raw.githubusercontent.com/telegramdesktop/tdesktop/dev/Telegram/SourceFiles/mtproto/scheme/api.tl',
+        {
+          method: 'GET',
+          mode: 'cors',
+        }
+      )
+    ).text()) +
+    '\n\n' +
+    layer;
   const re = /\/\/\s+LAYER\s+(\d+)/i;
   const reMd = /<b>Layer\s+(\d+)<\/b>/i;
-  const [fullTL, intTL] = tl.match(re);
+  const [fullTL, intTL] = layer.match(re);
   const [fullPr, intPr] = prev.match(re);
   const [fullMd, intMd] = readme.match(reMd);
   if (+intTL !== +intPr) {
     fs.writeFileSync(
       path.join(__dirname, '../api/source/api.tl'),
-      `// https://raw.githubusercontent.com/telegramdesktop/tdesktop/dev/Telegram/Resources/tl/api.tl\n${tl}`
+      `// https://raw.githubusercontent.com/telegramdesktop/tdesktop/dev/Telegram/SourceFiles/mtproto/scheme/api.tl\n${tl}`
     );
+    await notify.start(intTL);
   }
   if (+intTL !== +intMd) {
     fs.writeFileSync(
