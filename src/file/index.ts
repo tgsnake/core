07f8c8895d2988fa5831ef58e29143f625e284e6
butthx <68604099+butthx@users.noreply.github.com>
2023-12-19 20:54:07 +0700

    change: export type Progress

--- src/file/index.ts
+++ src/file/index.ts
@@ -9,5 +9,5 @@
  */
 
 export { File } from './File.ts';
-export { upload, Progress, SaveFileParams, SaveFileStreamParams } from './Upload.ts';
+export { upload, type Progress, SaveFileParams, SaveFileStreamParams } from './Upload.ts';
 export { handleDownload, downloadStream, DownloadParam } from './Download.ts';
