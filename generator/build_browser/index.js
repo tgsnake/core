dcf13b7ae7e6b22f7231f70acc74225edb176f30
butthx <68604099+butthx@users.noreply.github.com>
2026-05-29 20:24:29 +0700

    change github workflow for browser build testing and publishing package

--- generator/build_browser/index.js
+++ generator/build_browser/index.js
@@ -11,13 +11,27 @@ import fs from 'node:fs';
 import path from 'node:path';
 
 function buildForBrowser() {
+  console.log('Changing deps.ts for browser...');
+  const root = process.cwd();
+  const deps = path.join(root, 'deps.ts');
+  const depsNode = path.join(root, 'deps.node.ts');
+  const depsBrowser = path.join(root, 'deps.browser.ts');
+  if (!fs.existsSync(deps) && !fs.existsSync(depsBrowser)) return;
+  if (fs.existsSync(deps)) {
+    fs.renameSync(deps, depsNode);
+  }
+  if (fs.existsSync(depsBrowser)) {
+    fs.renameSync(depsBrowser, deps);
+  }
+  console.log('Deps changed for browser successfully!');
+  console.log('Changing package.json for browser...');
   const file = fs.readFileSync(path.join(process.cwd(), 'package.json'), 'utf8');
   const json = JSON.parse(file);
-  json.main = './browser/src/index.js';
-  json.scripts.prepare = json.scripts.build = 'node ./generator/bundler/index.mjs';
+  json.main = './browser/mod.js';
   json.files = ['browser/**/**'];
   json.type = 'module';
   json.version = `${json.version}-browser.0`;
   fs.writeFileSync(path.join(process.cwd(), 'package.json'), JSON.stringify(json, null, 2));
+  console.log('package.json changed for browser successfully!');
 }
 buildForBrowser();
